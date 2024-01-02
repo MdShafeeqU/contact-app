b2de5b52597f6df586e91d0bf95b44d8359da5bb
MdShafeeqU <mohamedshafeeq.usman@gmail.com>
2024-01-02 00:18:06 -0800

    Setting Props

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,31 @@ import './App.css';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
-import { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
+  
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts,contact]);
+  };
+  
+  // Store the data in local storage
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(contacts));
+  },[contacts]); //dependency
+
+  // Get the data from local storage
+  useEffect(() => {
+    localStorage.getItem(LOCAL_STORAGE_KEY);
+  },[contacts]);
+
   return (
     <div className="ui container">
       <Header/>
-      <AddContact />
+      <AddContact addContactHandler={addContactHandler}/>
       <ContactList contacts={contacts}/>
     </div>
   );
