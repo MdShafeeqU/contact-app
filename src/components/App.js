a884421fd66efd1edc8bbacaec8a645c66674f56
MdShafeeqU <mohamedshafeeq.usman@gmail.com>
2024-01-01 23:37:57 -0800

    adding states

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+import { useState } from 'react';
+
+function App() {
+  const [contacts, setContacts] = useState([]);
+  return (
+    <div className="ui container">
+      <Header/>
+      <AddContact />
+      <ContactList contacts={contacts}/>
+    </div>
+  );
+}
+
+export default App;
