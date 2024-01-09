ebdf0cc081fbf59280c5785ea59ca957eb705f83
MdShafeeqU <mohamedshafeeq.usman@gmail.com>
2024-01-08 22:09:05 -0800

    Adding props

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,75 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
+import { v4 as uuid } from 'uuid';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const LOCAL_STORAGE_KEY = "contacts";
-  const [contacts, setContacts] = useState([]);
-  
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []
+  );
+  // const [contacts, setContacts] = useState([]);
+
   const addContactHandler = (contact) => {
     console.log(contact);
-    setContacts([...contacts,contact]);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
   };
-  
-  // Store the data in local storage
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(contacts));
-  },[contacts]); //dependency
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+  };
 
   // Get the data from local storage
   useEffect(() => {
-    localStorage.getItem(LOCAL_STORAGE_KEY);
-  },[contacts]);
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retriveContacts) setContacts(retriveContacts);
+    console.log("here too");
+  }, []);
+
+  // Store the data in local storage
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+    console.log("here");
+  }, [contacts]); //dependency
 
   return (
     <div className="ui container">
-      <Header/>
-      <AddContact addContactHandler={addContactHandler}/>
-      <ContactList contacts={contacts}/>
+      <Router>
+      <Header />
+        <Routes>
+          {/* <Route 
+            path="/" 
+            render = {(props) => {
+               return <ContactList 
+                {...props} 
+                contacts={contacts} 
+                getContactId={removeContactHandler} 
+              />
+            }}
+          /> */}
+          <Route path="/" element={<ContactList contacts={contacts} getContactId={removeContactHandler} />} />
+          <Route path="/add" element={<AddContact addContactHandler={addContactHandler} />} />
+          {/* <Route  
+            path="/add" 
+            render = {(props) => (
+              <AddContact
+              {...props}
+              addContactHandler={addContactHandler}
+              />
+            )}
+          /> */}
+          
+          {/* <AddContact addContactHandler={addContactHandler} />
+        <ContactList contacts={contacts} getContactId={removeContactHandler} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Header = () =>{
     return (
-        <div className="ui fixed menu">
+        <div className="ui menu">
             <div className="ui container center">
-                <h2>Contact</h2>
+                <h2>Contact Manager</h2>
             </div>
         </div>
     )
